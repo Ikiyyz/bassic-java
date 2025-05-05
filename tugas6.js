13a546045c713fd9f516c3b53e63a653c6548eb5
Ikiyyz <rizkiyanaramdhani02@gmail.com>
2025-05-05 15:08:25 +0700

    feat: add recursive function to calculate digit multiplication

--- tugas6.js
+++ tugas6.js
@@ -1,11 +1,12 @@
 function sentencesManipulation(sentence) {
   const vokal = "aiueo";
-  const words = sentence.split(" "); 
-  console.log(words)
+  const words = sentence.split(" ");
+  console.log(words);
 
-  let hasil = []; 
+  let hasil = [];
 
-  for (let i = 0; i < words.length; i++) { // 0 < 6
+  for (let i = 0; i < words.length; i++) {
+    // 0 < 6
     const word = words[i];
 
     if (vokal.includes(word[0].toLowerCase())) {
@@ -14,9 +15,8 @@ function sentencesManipulation(sentence) {
       hasil.push(word.slice(1) + word[0] + "nyo");
     }
   }
-  return hasil.join(": ")
+  return hasil.join(" ");
 }
 
 console.log(sentencesManipulation("ibu pergi ke pasar bersama aku"));
 //console.log(sentencesManipulation("saya mau maen futsal"));
-
---
+++ tugas7.js
@@ -0,0 +1,20 @@
+function weirdMultipy(number) {
+  if (number < 10) {
+    return number;
+  }
+
+  let product = 1;
+
+  let digits = number.toString();
+  console.log(digits);
+
+  for (let i = 0; i < digits.length; i++) {
+    product *= parseInt(digits[i]);
+  }
+
+  return weirdMultipy(product);
+}
+
+console.log(weirdMultipy(39)); // Output: 4
+console.log(weirdMultipy(999)); // Output: 2
+console.log(weirdMultipy(3)); // Output: 3
