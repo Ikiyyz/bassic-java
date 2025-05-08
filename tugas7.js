3855a488c002a405aa889cbdfc75c8eadc6210d2
Ikiyyz <rizkiyanaramdhani02@gmail.com>
2025-05-08 13:10:31 +0700

    commit

--- tugas7.js
+++ tugas7.js
@@ -1,18 +1,18 @@
 function weirdMultipy(number) {
-  if (number < 10) {
+  if (number < 10) { 
     return number;
   }
 
   let product = 1;
 
-  let digits = number.toString();
+  let digits = number.toString(); "14"
   console.log(digits);
 
   for (let i = 0; i < digits.length; i++) {
-    product *= parseInt(digits[i]);
+    product *= parseInt(digits[i]); 
   }
 
-  return weirdMultipy(product);
+  return weirdMultipy(product); 
 }
 
 console.log(weirdMultipy(39)); // Output: 4
---
+++ tugas8.js
@@ -0,0 +1,20 @@
+function pola(str) {
+  const [left, right] = str.split(" = "); //["42#3 * 188", "80#204"]
+  const [num1, num2] = left.split(" * "); //["42#3", "188"]
+
+  for (let i = 0; i <= 9; i++) {
+    for (let j = 0; j <= 9; j++) {
+      const newNum1 = num1.replace("#", i); // "42#3"
+      const newRight = right.replace("#", j); // "80#204"
+
+      //console.log(newNum1, newRight);
+
+      if (parseInt(newNum1) * parseInt(num2) === parseInt(newRight)) {
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
+console.log(pola("42#3 * 188 = 80#204")); // [8, 5]
